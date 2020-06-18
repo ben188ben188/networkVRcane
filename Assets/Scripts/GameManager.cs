fd07e39253f103a26073fc16e7eb9b21683ab2a5
Kelvin <54338129+Breathing-ls-Fun@users.noreply.github.com>
2020-06-18 15:38:37 -0400

    -

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -34,10 +34,7 @@ namespace Com.MyCompany.MyGame
                 {
                     Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
                     // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-                    GameObject playerInstance =  PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
-
-                    playerInstance.transform.parent = GameObject.FindGameObjectWithTag("ARSessionOrigin").transform;
-                    
+                    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
                 }
                 else
                 {
